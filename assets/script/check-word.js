6d5f35ad7cd7f13eb0fa5c598f85aa49b9dd20bb
nick.gnmc <nick.sutton@gbmc.ac.uk>
2025-08-05 18:10:46 +0100

    Add function to check word exists
    Use the datamuse API

--- assets/script/check-word.js
+++ assets/script/check-word.js
@@ -1,2 +1,28 @@
 /*global document*/
 
+async function wordExists(word) {
+    let query = "https://api.datamuse.com/words?sp=" + word + "&max=1";
+    const response = await fetch(query);
+    const data = await response.json();
+    let found = false;
+    if (data.length == 0) {
+        found = false;
+    } else {
+        if (data[0].score > 10) {
+            found = true;
+        }
+        else {
+            found = false;
+        }
+    }
+    return found;
+}
+
+async function testWord() {
+    let word = document.getElementById("word").value;
+    if (await wordExists(word)) {
+        document.getElementById().innerHTML = "Word found";
+    } else {
+        document.getElementById().innerHTML = "Word found";
+    }
+}
